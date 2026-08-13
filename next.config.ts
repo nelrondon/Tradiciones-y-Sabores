cacdf187382a24c5bad1522aa0fc75c636302f3a
justcarlux <manuelitojuan691@gmail.com>
2026-08-13 17:28:49 -0400

    fix(next-config): :bug: hardcode route rewrites to properly query the backend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: "http://backend:3000/api/v1/:path*"
+      }
+    ];
+  }
 };
 
 export default nextConfig;

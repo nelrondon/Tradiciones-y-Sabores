475beab7647e58fa6992493450e5fbf00c4d71a5
justcarlux <manuelitojuan691@gmail.com>
2026-08-13 17:44:35 -0400

    we don't need this anymore

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: "http://backend:3000/api/v1/:path*"
-      }
-    ];
-  }
+  output: "standalone"
 };
 
 export default nextConfig;
